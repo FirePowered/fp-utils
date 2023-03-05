be74f6bbbe644629bd6f00996fae8f8a33a916c9
Kyle Smith <smith.kyle1734@gmail.com>
2023-03-05 11:08:43 -0500

    Create methods for generating random strings

--- src/main/java/org/firepowered/core/utils/StringUtils.java
+++ src/main/java/org/firepowered/core/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package org.firepowered.core.utils;
 
+import java.util.Random;
+
 /**
  * Common {@link String} utility methods. All methods can handle {@code null}s
  * safely, further details in their javadocs.
@@ -8,6 +10,30 @@ package org.firepowered.core.utils;
  * @since 1.0
  */
 public final class StringUtils {
+
+    private static final String ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHA_LOWER = ALPHA_UPPER.toLowerCase();
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIALS = "!@#$%^&*_-";
+
+    private static final String ALPHANUMERIC = ALPHA_UPPER + ALPHA_LOWER + NUMBERS;
+
+    /**
+     * Flag indicating to include alphabetical characters (letters) when generating
+     * a string.
+     */
+    public static final int RANDOM_ALPHA = 1 << 0;
+
+    /**
+     * Flag indicating to include numbers when generating a string.
+     */
+    public static final int RANDOM_NUM = 1 << 1;
+
+    /**
+     * Flag indicating to include special characters (!@#$%^&*_-) when generating a
+     * string.
+     */
+    public static final int RANDOM_SPEC = 1 << 2;
 
     private StringUtils() {
     }
@@ -19,10 +45,75 @@ public final class StringUtils {
      * @param str The string, may be {@code null} as {@code null} strings are
      *            considered empty
      * @return True if the string is empty, false otherwise
-     * @since 1.0
      */
     public static boolean isEmpty(String str) {
         return str == null || str.strip().length() == 0;
     }
 
+    private static String randomStringInternal(int length, String chars) {
+        assert length > 0 : "A non-zero length must be provided";
+        if (isEmpty(chars)) {
+            // This is an internal error that should never be reached
+            throw new RuntimeException("Empty charset");
+        }
+        char[] res = new char[length];
+        Random rng = new Random();
+        for (int i = 0; i < length; i++) {
+            int idx = rng.nextInt(chars.length());
+            res[i] = chars.charAt(idx);
+        }
+        return new String(res);
+    }
+
+    /**
+     * Generates a random string of the given length. This version only includes
+     * alphanumeric characters ({@code [A-Za-z0-9]}).
+     * 
+     * @param length Length of the string, must be greater than 0
+     * @return A random string
+     */
+    public static String randomStringAlphaNum(int length) {
+        return randomStringInternal(length, ALPHANUMERIC);
+    }
+
+    /**
+     * Generates a random string of the given length with the given settings. The
+     * {@code mode} argument is a bitstring containing values from {@code RANDOM_*}
+     * to determine which characters should be included in the generated string.
+     * <p>
+     * If {@code mode} is less than 0, {@link #randomStringAlphaNum(int)} will be
+     * called to generate a random alphanumeric string as the default.
+     * 
+     * @param length Length of the string to generate, must be greater than 0
+     * @param mode   Bitstring of {@code RANDOM_*} flags defined in this class
+     * @return A random string
+     * @throws IllegalArgumentException If {@code mode} doesn't include any of the
+     *                                  flags
+     * @see StringUtils#RANDOM_ALPHA
+     * @see StringUtils#RANDOM_NUM
+     * @see StringUtils#RANDOM_SPEC
+     */
+    public static String randomString(int length, int mode) {
+        if (mode <= 0) {
+            return randomStringAlphaNum(length);
+        }
+        String chars = "";
+        if ((mode & RANDOM_ALPHA) == RANDOM_ALPHA) {
+            chars += ALPHA_UPPER + ALPHA_LOWER;
+        }
+
+        if ((mode & RANDOM_NUM) == RANDOM_NUM) {
+            chars += NUMBERS;
+        }
+
+        if ((mode & RANDOM_SPEC) == RANDOM_SPEC) {
+            chars += SPECIALS;
+        }
+
+        if (isEmpty(chars)) {
+            throw new IllegalArgumentException(
+                    "Invalid mode bitstring as no character sets were selected for inclusion");
+        }
+        return randomStringInternal(length, chars);
+    }
 }
--- src/test/java/org/firepowered/core/utils/StringUtilsTest.java
+++ src/test/java/org/firepowered/core/utils/StringUtilsTest.java
@@ -1,10 +1,21 @@
 package org.firepowered.core.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Testing {@link StringUtils}.
+ * 
+ * @author Kyle Smith (smith.kyle1734@gmail.com)
+ * @since 1.0
+ */
+@SuppressWarnings("javadoc")
 public class StringUtilsTest {
 
     @Test
@@ -15,4 +26,43 @@ public class StringUtilsTest {
         assertTrue(StringUtils.isEmpty("     "));
     }
 
+    @Test
+    public void testRandomString_default() {
+        int mode = 0;
+        String random = StringUtils.randomString(5, mode);
+        assertTrue(Pattern.matches("[A-Za-z0-9]+", random));
+
+        final int finalMode = 4096;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> StringUtils.randomString(5, finalMode));
+    }
+
+    @Test
+    public void testRandomString_alpha() {
+        int mode = StringUtils.RANDOM_ALPHA;
+        String random = StringUtils.randomString(5, mode);
+        assertTrue(Pattern.matches("[A-Za-z]{5}", random));
+    }
+
+    @Test
+    public void testRandomString_num() {
+        int mode = StringUtils.RANDOM_NUM;
+        String random = StringUtils.randomString(5, mode);
+        Integer.valueOf(random);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testRandomString_spec() {
+        int mode = StringUtils.RANDOM_SPEC;
+        String random = StringUtils.randomString(5, mode);
+        assertTrue(Pattern.matches("[!@#$%^&*-_]{5}", random));
+    }
+
+    @Test
+    public void testRandomStringAlphaNum() {
+        String rand = StringUtils.randomStringAlphaNum(5);
+        assertEquals(5, rand.length());
+
+        Assertions.assertThrows(AssertionError.class, () -> StringUtils.randomStringAlphaNum(0));
+    }
 }
